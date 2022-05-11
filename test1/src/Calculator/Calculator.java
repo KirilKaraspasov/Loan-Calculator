00d04fa02c7af6aaf4a5c7172780339c011ea661
KirilKaraspasov <47929445+KirilKaraspasov@users.noreply.github.com>
2022-05-11 22:01:15 +0300

    Create Calculator.java
    create calculator app

---
+++ test1/src/Calculator/Calculator.java
@@ -0,0 +1,36 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Въведете размер на заема");
+        double sumOfLoan = Double.parseDouble(scanner.nextLine());
+        System.out.println("Въведете срок на заема");
+        double lenghtOfLoan = Double.parseDouble(scanner.nextLine());
+        System.out.println("Въведете годишна лихва на заема");
+        double interestOfloan = Double.parseDouble(scanner.nextLine());
+        double yearInterest = interestOfloan / 12;
+
+
+        double totalSum = sumOfLoan + yearInterest;
+        double totalInterestOfLoan = lenghtOfLoan * yearInterest;
+        double sumPerMonth = (sumOfLoan + totalInterestOfLoan) / lenghtOfLoan ;
+
+
+        System.out.printf("Месечна вноска");
+        System.out.println(sumPerMonth);
+
+        System.out.printf("Общо лихва за период");
+        System.out.println(totalInterestOfLoan);
+
+        System.out.printf("Обща сума за погасяване");
+        System.out.println(totalSum);
+
+
+    }
+}
